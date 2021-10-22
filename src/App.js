5ec9bcfc4bbf6b5f02297db6851bd1bcdcd947bb
Tanya <ta.rohlova@yandex.ru>
2021-10-22 21:36:33 +0300

    исп useState

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React, { Component } from 'react'
+import React, {useState} from 'react'
 import TodoList from './TodoList'
 
-export default class App extends Component {
-  state = {
-    todos: [
-      {id: 1, title: 'First todo', completed: false},
-      {id: 2, title: 'Second todo', completed: true},
-    ]
+export default function App() {
+  const [todos, setTodos] = useState([
+    {id: 1, title: 'First todo', completed: false},
+    {id: 2, title: 'Second todo', completed: true},
+  ])
+  const [todoTitle, setTodoTitle] = useState('')
+
+  //Доб. в список
+  const addTodo = event => {
+    //если нажата клавиша Enter
+    if(event.key === 'Enter') {
+      // в setTodos передаем:
+      setTodos([
+        ...todos, //1)старые элементы
+        //2) новые элементы
+        {
+          id: Date.now(),
+          title: todoTitle,
+          completed: false
+        }
+      ])
+      setTodoTitle('') //очищаем инпут
+    }
   }
 
-  render() {
-    return (
-      <div className="container">
-        <h1>To do app</h1>
+  return (
+    <div className="container">
+      <h1>Todo app</h1>
 
-          <div className="input-field">
-            <input type="text" />
-            <label>Todo name</label>
-          </div>
+        <div className="input-field">
+          <input 
+            type="text"
+            value={todoTitle}
+            onChange={event => setTodoTitle(event.target.value)}
+            onKeyPress={addTodo}
+            />
+          <label>Todo name</label>
+        </div>
 
-          <TodoList todos={this.state.todos} />
-      </div>
-    );
-  }
+        <TodoList todos={todos} />
+    </div>
+  );
 }
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function TodoItem({title, id, completed}) {
+  const [checked, setChecked] = useState(completed)
+
+  const cls = ['todo']
+
+  //если checked = true
+  if (checked) {
+    cls.push('completed') //то в массив классов доб. класс completed
+  }
   return (
-    <li className="todo">
+    <li className={cls.join(' ')}>
       <label>
         <input
           type="checkbox"
-          defaultChecked={false}
+          checked={checked}
+          onChange={() => setChecked(!checked)}
         />
         <span>{title}</span>
 
